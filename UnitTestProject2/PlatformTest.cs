d25b1c71c12b23310c91518f1df977bdefac19cc
jeps <petteri.stenius@ubisecure.com>
2017-09-28 17:25:25 +0300

    updates

---
+++ UnitTestProject2/PlatformTest.cs
@@ -0,0 +1,47 @@
+﻿using Jose;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace UnitTestProject2
+{
+    public class PlatformTest
+    {
+        [Fact]
+        public void RsaTest()
+        {
+            JweAlgorithm[] algorithms = {
+                JweAlgorithm.RSA1_5,
+                JweAlgorithm.RSA_OAEP,
+                //JweAlgorithm.RSA_OAEP_256
+            };
+            JweEncryption[] content =
+            {
+                JweEncryption.A256CBC_HS512,
+                JweEncryption.A192CBC_HS384,
+                JweEncryption.A128CBC_HS256,
+                //JweEncryption.A128GCM,
+            };
+            using (var key = RSA.Create(2048))
+            {
+                foreach (JweAlgorithm alg in algorithms)
+                {
+                    foreach (JweEncryption enc in content)
+                    {
+                        try
+                        {
+                            string jws = JWT.Encode("hello world", key, JwsAlgorithm.RS256);
+                            string jwe = JWT.Encode(jws, key, alg, enc);
+                            Assert.NotNull(jwe);
+                        } catch(Exception e)
+                        {
+                            throw new ApplicationException("alg=" + alg + ",enc=" + enc, e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ jose-jwt/jwk/JsonWebKey.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Jose.jwk.util;
+
+namespace Jose.jwk
+{
+    public class JsonWebKey
+    {
+        public static JsonWebKey Parse(string json, JwtSettings settings = null)
+        {
+            settings = settings ?? JWT.DefaultSettings;
+            IDictionary<string, object> header = settings.JsonMapper
+                .Parse<IDictionary<string, object>>(json);
+            return new JsonWebKey(header);
+        }
+        public JsonWebKey(IDictionary<string,object> header)
+        {
+            Header = header;
+        }
+        public IDictionary<string, object> Header { get; private set; }
+        public string KeyType
+        {
+            get => Header.GetString("kty");
+            //set => Header.Set("kty", value);
+        }
+        public string Use
+        {
+            get => Header.GetString("use");
+            //set => Header.Set("use", value);
+        }
+        public bool IsSignature { get => string.Equals("sig", Use ?? "sig"); }
+        public bool IsEncryption { get => string.Equals("enc", Use ?? "enc"); }
+        public string Id
+        {
+            get => Header.GetString("kid");
+            //set => Header.Set("kid", value);
+        }
+        public bool TryGetSignatureKey<T>(out T key)
+        {
+            key = default(T);
+            return false;
+        }
+        public bool TryGetEncryptionKey<T>(out T key)
+        {
+            key = default(T);
+            return false;
+        }
+    }
+}
